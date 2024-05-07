2616f5a003cde1fd8748c2af4193aa97080ae33d
Ilham Atsaris <ilhamatsaris@gmail.com>
2024-05-08 06:49:33 +0700

    add script for build and push docker image

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#membuat docker image dari docker file
+docker build -t ghcr.io/ilhamatsaris20/karsajobs-ui:latest .
+#login ke github container registry
+echo $PASSWORD_DOCKER_HUB | docker login -u ilhamatsaris20 --password-stdin ghcr.io
+#upload docker image  ke github
+docker push ghcr.io/ilhamatsaris20/karsajobs-ui:latest
