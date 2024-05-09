dbeeb71b1385aa41aa695276d1a38ef8dea0f67c
Ilham Atsaris <ilhamatsaris@gmail.com>
2024-05-09 16:03:46 +0700

    change username github for push docker images to github container registry

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #membuat docker image dari docker file
-docker build -t ghcr.io/ilhamatsaris20/karsajobs:latest .
+docker build -t ghcr.io/ilhamatsaris/karsajobs:latest .
 #login ke github container registry
-echo $PASSWORD_DOCKER_HUB | docker login -u ilhamatsaris20 --password-stdin ghcr.io
+echo $PASSWORD_DOCKER_HUB | docker login -u ilhamatsaris --password-stdin ghcr.io
 #upload docker image ke github
-docker push ghcr.io/ilhamatsaris20/karsajobs:latest
+docker push ghcr.io/ilhamatsaris/karsajobs:latest
